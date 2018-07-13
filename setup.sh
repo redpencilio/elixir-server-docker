62043171dfafb35636975e61009b978eee21661e
Aad Versteden <madnificent@gmail.com>
2018-07-13 22:13:43 +0200

    More efficient compilation
    The new image has a more efficient compilation strategy.  It takes a
    while longer to compile, but it's made for production runs.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 cd /app
 
-yes | MIX_ENV=prod mix do deps.get, compile
+echo "Getting deps"
+yes | MIX_ENV=prod mix deps.get
 
+echo "Fancy compile"
+MIX_ENV=prod elixir $CMD_OPTS -pa _build/prod/consolidated -S mix compile
+
+# echo "Compiling deps"
+yes | MIX_ENV=prod mix deps.compile
+echo "Compiling app"
+yes | MIX_ENV=prod mix compile
+
+echo "Compiling protocols"
 MIX_ENV=prod mix compile.protocols
+
+echo "Done setting up"
