b0b54bb2fc51e642a282631cf164b7a40c66beae
Aad Versteden <madnificent@gmail.com>
2019-10-09 11:21:35 +0200

    Allow specifying port on which the process should run
    Erlang chooses an open port in a range when starting a new node.  We
    can force the port if desired.  This may be handy if port forwarding
    is desired.

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ fi
 
 IP_ADDRESS=`ip addr show eth0 | grep inet | grep -oP "\\d+.\\d+.\\d+.\\d+" | head -n 1`
 CMD_OPTS=""
+ERL_OPTS=""
 
 if [[ $IEX_NAME ]]
 then
@@ -19,6 +20,11 @@ then
     CMD_OPTS="$CMD_OPTS --cookie $IEX_COOKIE"
 fi
 
+if [[ $IEX_PORT ]]
+then
+    ERL_OPTS="$ERL_OPTS -kernel inet_dist_listen_min $IEX_PORT -kernel inet_dist_listen_max $IEX_PORT"
+fi
+
 cd /app
 
-MIX_ENV=prod elixir $CMD_OPTS -pa _build/prod/consolidated -S mix run --no-halt
+MIX_ENV=prod elixir $CMD_OPTS --erl "$ERL_OPTS" -pa _build/prod/consolidated -S mix run --no-halt
